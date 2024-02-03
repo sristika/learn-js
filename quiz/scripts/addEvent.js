c13e47b7452f81459625be95efa1650d005ade06
sristika <110280812+sristika@users.noreply.github.com>
2024-02-02 22:58:15 -0500

    Update

--- quiz/scripts/addEvent.js
+++ quiz/scripts/addEvent.js
@@ -3,16 +3,21 @@ window.onload = function() {
     x.addEventListener('mouseover', myFunction('Moused over!'));
     x.addEventListener('click', mySecondFunction('Clicked!'));
     x.addEventListener('mouseout', myThirdFunction('Moused out!'));
+    x.addEventListener('mouseover', () => changeFunction('Moused over!'));
+    x.addEventListener('click', () => changeFunction('Clicked!'));
+    x.addEventListener('mouseout', () => changeFunction('Moused out!'));
+}
+
+function changeFunction(msg) {
+    document.getElementById('demo').textContent = msg;
 }
 
 function myFunction(msg) {
     document.getElementById('demo').textContent = msg;
 }
-
 function mySecondFunction(msg) {
     document.getElementById('demo').textContent = msg;
 }
-
 function myThirdFunction(msg) {
     document.getElementById('demo').textContent = msg;
 }
