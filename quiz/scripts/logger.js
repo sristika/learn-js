dd8b7b3d56bf9ee52cff4790aa20e3d035b44d29
sristika <borasristika.new@gmail.com>
2024-02-05 17:40:08 -0500

    updates

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,13 @@
-// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg.
+const logger = function (obj) {
+  if (obj.errMsg) {
+    console.log(obj.errMsg);
+  }
+};
+
+const temp = {
+  name: 'Sristika',
+  errMsg: 'This is an error message',
+};
+
+logger(temp);
